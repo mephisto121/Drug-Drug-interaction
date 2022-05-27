01bc4d4d078d7f5353d566d18a334cd782344dfa
mephisto121 <71381384+mephisto121@users.noreply.github.com>
2022-05-27 08:17:28 +0430

    Update main.py

--- main.py
+++ main.py
@@ -68,4 +68,6 @@ if __name__ == '__main__':
     
     predict = model_predict.predict([d1_ids, d2_ids, d1_mask, d2_mask])
     number = tf.argmax(predict, axis = 1)
+    print("---------------------------------------------------------------------------------")
+    print(f"The labels is equal to {number}")
     print(labels['label_name'][number].values)
